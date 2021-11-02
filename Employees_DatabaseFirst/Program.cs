9e326e6a0ab3282406848de73fff32619029236e
first <Katya_Klebleeva@mail.ru>
2021-11-02 13:55:19 +0300

    Add project and .gitignore file

---
+++ Employees_DatabaseFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employees_DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
